8964d1f05fc9f5362d8332329150d397f91e450a
jluce1996 <jluce1996@gmail.com>
2013-09-10 08:40:53 -0400

    pizza project

---
+++ ch7problems/problem1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza Italian = new Pizza();
+            Italian.ItemPrice = 13.99;
+            Italian.Toppings = "pepperoni";
+            Italian.Diameter = 12;
+        }
+    }
+}
---
+++ ch7problems/problem1/TestPizza.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem1
+{
+    class Pizza
+    {        
+        string toppings;
+        int diameter;
+        double itemPrice;
+
+        public string Toppings
+        {
+            get
+            {
+                return toppings;
+            }
+            set
+            {
+                toppings = value;
+            }
+        }
+
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+
+        public double ItemPrice
+        {
+            get
+            {
+                return itemPrice;
+            }
+            set
+            {
+                itemPrice = value;
+            }
+        }
+
+
+
+
+    }
+}
