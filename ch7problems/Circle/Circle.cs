737d31468dca6f4e2463047da8fbb756ed1c1b04
jluce1996 <jluce1996@gmail.com>
2013-09-11 08:28:22 -0400

    circles, project tres

---
+++ ch7problems/Circle/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Circle
+{
+    class Circle
+    {
+        double radius;
+        double area;
+        double diameter;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+                diameter = radius * 2;
+                area = radius * radius * Math.PI;
+            }
+        }
+
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+        }
+
+
+        
+
+
+            
+    }
+}
---
+++ ch7problems/Circle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle Round, Rounder, Roundest;
+
+
+            Round = new Circle();
+            Round.Radius = .5;
+
+            Rounder = new Circle();
+            Rounder.Radius = 4;
+
+            Roundest = new Circle();
+            Roundest.Radius = 1;
+
+            Console.WriteLine(Round.Radius + " " + Round.Diameter + " " + Round.Area + " ");
+            Console.WriteLine(Rounder.Radius + " " + Rounder.Diameter + " " + Rounder.Area + " ");
+            Console.WriteLine(Roundest.Radius + " " + Roundest.Diameter + " " + Roundest.Area + " ");    
+                
+
+        }
+    }
+}
