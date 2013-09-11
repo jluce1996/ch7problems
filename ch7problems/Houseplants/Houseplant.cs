467d4f7eb8fc9692c1705aa8ecbb074cc1a8f251
jluce1996 <jluce1996@gmail.com>
2013-09-11 07:46:18 -0400

    created plants, project dos

---
+++ ch7problems/Houseplants/Houseplant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Houseplants
+{
+    class Houseplant
+    {
+        string plantname;
+        double price;
+        bool isfed;
+        
+        public string Plantname
+        {
+            get
+            {
+            return plantname;
+            }
+            set
+            {
+            plantname = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public bool Isfed
+        {
+            get
+            {
+                return isfed;
+            }
+            set
+            {
+                isfed = value;
+            }
+        }
+
+
+    }
+}
---
+++ ch7problems/Houseplants/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Houseplants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Houseplant Daisy, Rose, Tulip;
+
+            Daisy = new Houseplant();
+            Daisy.Plantname = "daisy";
+            Daisy.Price = 13.99;
+            Daisy.Isfed = true;
+
+            Rose = new Houseplant();
+            Rose.Plantname = "rose";
+            Rose.Price = 15.99;
+            Rose.Isfed = true;
+
+            Tulip = new Houseplant();
+            Tulip.Plantname = "tulip";
+            Tulip.Price = 20.99;
+            Tulip.Isfed = true;
+
+
+
+            }
+        }
+    }
+
