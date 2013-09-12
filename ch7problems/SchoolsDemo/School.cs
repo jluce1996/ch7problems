d950aa9aba4a09471d0a8e22dc3304d570681c25
jluce1996 <jluce1996@gmail.com>
2013-09-12 08:39:30 -0400

    school project beginning, project cinco dos

---
+++ ch7problems/SchoolsDemo/School.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchoolsDemo
+{
+    class School : IComparable
+    {
+        public string schoolname {get; set;}
+        public int enrollment {get; set;}
+
+        int IComparable.CompareTo(Object o)
+        {
+            int returnVal;
+            School temp = (School)o;
+            if(this.enrollment > temp.enrollment)
+                    returnVal = 1;
+            else
+              if(this.enrollment < temp.enrollment)
+                    returnVal = -1;
+              else
+                returnVal = 0;
+            return returnVal;
+        }
+    }
+}
