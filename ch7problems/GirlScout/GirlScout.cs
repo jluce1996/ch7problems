d4ecf45514da7e2752928159f6a97248f56335cb
jluce1996 <jluce1996@gmail.com>
2013-09-12 08:10:32 -0400

    girl scout project, project cinco

--- ch7problems/GirlScout/GirlScout.cs
+++ ch7problems/GirlScout/GirlScout.cs
@@ -7,5 +7,23 @@ namespace GirlScout
 {
     class GirlScout
     {
+        public string name { get; set; }
+        public int number { get; set;}
+        public double dues { get; set; }
+        public const string motto = "to obey the Girl Scout Law";
+
+        public GirlScout()
+        {
+            name = "Sarah"; 
+            number = 3509;
+            dues = 19.99;
+        }
+
+        public GirlScout(string name, int number, double dues)
+        {
+            this.name = name;
+            this.number = number;
+            this.dues = dues;
+        }
     }
 }
--- ch7problems/GirlScout/Program.cs
+++ ch7problems/GirlScout/Program.cs
@@ -9,6 +9,17 @@ namespace GirlScout
     {
         static void Main(string[] args)
         {
+            GirlScout scout = new GirlScout();
+            scout.name = "Lacy";
+            scout.dues = 13.01;
+            scout.number = 3410;
+
+            GirlScout ranger = new GirlScout("Victoria", 3410, 12.99);
+
+            Console.WriteLine(scout.name + "" + scout.dues + "" + scout.number + "");
+            Console.WriteLine(ranger.name + "" +  ranger.dues + "" +  ranger.number + "");
+            Console.WriteLine(GirlScout.motto);
+
         }
     }
 }
