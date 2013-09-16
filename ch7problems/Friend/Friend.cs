4cd0cfd53b3f5ae2e6b31a33a1f53e93b5a7567d
jluce1996 <jluce1996@gmail.com>
2013-09-16 08:41:56 -0400

    beginning of friend se

---
+++ ch7problems/Friend/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Friend
+{
+    class Friend
+    {
+        public int phone {get; set;}
+        public int month {get; set;}
+        public int day {get; set;}
+        public int year {get; set;}
+        public int birthday {get; set;} 
+        
+    }
+}
---
+++ ch7problems/Friend/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Friend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int i;
+            string name;
+
+            Friend[] friend;
+            friend = new Friend[7];
+            Console.Write("Enter 8 friends names:");
+            input = Console.ReadLine();
+            Array.Sort(friend);
+            Console.WriteLine("Sorted List:");
+            for (i = 0; i < friend.Length; i++)
+            {
+                Console.WriteLine(friend[i].name);
+            } 
+
+        }
+    }
+}
