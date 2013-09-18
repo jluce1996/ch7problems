ad735a3e72253687f18212b20c8c9ab0d6ff0fa9
jluce1996 <jluce1996@gmail.com>
2013-09-18 08:39:42 -0400

    second middle of friend se

--- ch7problems/Friend/Friend.cs
+++ ch7problems/Friend/Friend.cs
@@ -7,25 +7,27 @@ namespace Friend
 {
     class Friend : IComparable 
     {
-        public int phone {get; set;}
+        public string phone {get; set;}
         public int month {get; set;}
         public int day {get; set;}
         public int year {get; set;}
         public int birthday {get; set;}
         public string name;
 
-        public Friend(string name, int month, int year, int day)
+        public Friend(string name, int month, int year, int day, string phone)
         {
             this.name = name;
             this.month = month;
             this.year = year;
             this.day = day;
+            this.phone = phone;
         }
 
         int IComparable.CompareTo(Object o)
         {
-            
-            return this.name.CompareTo(o);
+            Friend friend = (Friend)o;
+
+            return this.name.CompareTo(friend.name);
         }
     }
 }
--- ch7problems/Friend/Program.cs
+++ ch7problems/Friend/Program.cs
@@ -7,7 +7,7 @@ namespace Friend
 {
     class Program
     {
-        public static void GetData(out string name, out int month, out int day, out int year)
+        public static void GetData(out string name, out int month, out int day, out int year, out string phone)
         {
             string input;
             
@@ -27,21 +27,29 @@ namespace Friend
                 year = Convert.ToInt32(parts[2]);
             }
 
+            Console.Write("Enter their phone number (000-000-0000):");
+
+            phone = Console.ReadLine();
+            
+                
         }
         static void Main(string[] args)
         {
             string name;
             int month, day, year;
             int i;
+            string phone;
+            
+            
 
             Friend[] friend;
-            friend = new Friend[7];
+            friend = new Friend[3];
             
             for(i = 0; i < friend.Length; i++)
             {
-                GetData(out name, out month, out day, out year);
+                GetData(out name, out month, out day, out year, out phone);
 
-                friend[i] = new Friend(name, month, day, year);
+                friend[i] = new Friend(name, month, day, year, phone);
             }
 
 
@@ -50,8 +58,18 @@ namespace Friend
             for (i = 0; i < friend.Length; i++)
             {
                 Console.WriteLine(friend[i].name);
-            } 
+            }
 
+            Console.Write("Enter a specific friend's name:");
+                name = Console.ReadLine();
+
+                for (i = 0; i < friend.Length; i++)
+                {
+                    if (name == friend[i].name)
+                    {
+                        Console.WriteLine("Phone: {0}, Birthday: {1}/{2}/{3}", friend[i].phone, friend[i].month, friend[i].day, friend[i].year); 
+                    }
+                }
         }
     }
 }
